5855ef3646cfa7d578f7782b9aa8ad90633d9537
Tim Shedor <tshedor@gmail.com>
2016-02-13 10:20:17 -0800

    unminify JS

--- config.rb
+++ config.rb
@@ -33,7 +33,7 @@ configure :build do
   activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
 
   # Enable cache buster
   # https://github.com/middleman/middleman/issues/480
